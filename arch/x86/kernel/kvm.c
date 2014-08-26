991d2b63f65665d297a2c03be326a0bd15d08b40
Stephen Rothwell <sfr@canb.auug.org.au>
2014-08-26 17:09:29 +1000

    Revert "kvm: ensure hard lockup detection is disabled by default"
    This reverts commit 1e76d85beb1b0aba10bbdf275af9e08e9384da40.

--- arch/x86/kernel/kvm.c
+++ arch/x86/kernel/kvm.c
@@ -35,7 +35,6 @@
 #include <linux/slab.h>
 #include <linux/kprobes.h>
 #include <linux/debugfs.h>
-#include <linux/nmi.h>
 #include <asm/timer.h>
 #include <asm/cpu.h>
 #include <asm/traps.h>
@@ -500,13 +499,6 @@ void __init kvm_guest_init(void)
 #else
 	kvm_guest_cpu_init();
 #endif
-
-	/*
-	 * Hard lockup detection is enabled by default. Disable it, as guests
-	 * can get false positives too easily, for example if the host is
-	 * overcommitted.
-	 */
-	watchdog_enable_hardlockup_detector(false);
 }
 
 static noinline uint32_t __kvm_cpuid_base(void)
